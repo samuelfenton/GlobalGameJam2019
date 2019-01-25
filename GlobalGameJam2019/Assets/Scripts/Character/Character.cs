c0e5d389b63b7c368a39cc4c64c95a5903fa4ed7
SamuelFenton <samuel.fenton@outlook.com>
2019-01-25 22:02:38 +1100

    Updated player, renamed character

---
+++ GlobalGameJam2019/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Header("Movement")]
+    public float m_movementAcceleration = 1.0f;
+    public float m_maxSpeed = 1.0f;
+
+    protected Rigidbody m_rigidbody = null;
+    protected virtual void Start ()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- GlobalGameJam2019/Assets/Scripts/Character/Player/Player.cs
+++ GlobalGameJam2019/Assets/Scripts/Character/Player/Player.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : Character
+{
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        m_rigidbody = GetComponent<Rigidbody>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        //Basic movemnt
+        Vector3 frameVelocity = m_rigidbody.velocity + new Vector3(Input.GetAxisRaw("Horizontal") * m_movementAcceleration, 0.0f, Input.GetAxisRaw("Vertical") * m_movementAcceleration) * Time.deltaTime;
+
+        frameVelocity = frameVelocity.normalized * m_maxSpeed;
+
+        m_rigidbody.velocity = frameVelocity;
+    }
 }
