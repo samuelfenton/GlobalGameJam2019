bc2c0056c4297d6f9c26dfa96db2c8cbf3e50a18
Hypr0 <eddie.ultra@gmail.com>
2019-01-26 09:32:36 +1100

    added score managing

---
+++ GlobalGameJam2019/Assets/Scripts/Gameplay/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ScoreManager : MonoBehaviour {
+
+    //current highscore
+    float fCurrentScore = 0.0f;
+    //current name of player
+    string szPlayerName = "tst";
+
+    //file location
+    string szScoreTextFile = "Assets/Scripts/Gameplay/scores.txt";
+
+    //Game Manager reference
+    public GameManager g;
+
+    void Awake()
+    {
+        saveScore();
+    }
+
+    void Update ()
+    {
+        fCurrentScore = g.nAlcoholBottles;
+	}
+
+    //saves the score into a notepad fle
+    void saveScore()
+    {
+        StreamWriter write = new StreamWriter(szScoreTextFile, true);
+        write.WriteLine(szPlayerName + ": " + fCurrentScore.ToString());
+        write.Close();
+    }
+}
