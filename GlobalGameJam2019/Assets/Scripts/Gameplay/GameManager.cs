1dbdca1af3a338786caf99e9d9dca757ac895db9
Hypr0 <eddie.ultra@gmail.com>
2019-01-26 12:38:17 +1100

    added simplified loop for selecting drunk effects

--- GlobalGameJam2019/Assets/Scripts/Gameplay/GameManager.cs
+++ GlobalGameJam2019/Assets/Scripts/Gameplay/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour {
     public int nAlcoholBottles = 0;
 
 
-    public float[] effectPercentages = new float[2];
+    public float[] effectPercentages = new float[(int)Player.DRUNK_EFFECTS.EFFECT_COUNT];
 
     //player reference
     public Transform tPlayerPosition;
@@ -30,52 +30,58 @@ public class GameManager : MonoBehaviour {
     {
         bool[] currentDrunkenness = new bool[(int)Player.DRUNK_EFFECTS.EFFECT_COUNT];
         //Do magic here
+        //initialising base variables
+        for (int i = 0; i < (int)Player.DRUNK_EFFECTS.EFFECT_COUNT; i++)
+        {
+            currentDrunkenness[i] = false;
+        }
+        
 
         switch (nAlcoholBottles)
         {
             case 1:
                 {
-                    currentDrunkenness = PlayerEffectedCalc(currentDrunkenness, 10.0f);
+                    currentDrunkenness = temp(currentDrunkenness, 10.0f);
                 }
                 break;
             case 2:
                 {
-                    currentDrunkenness = PlayerEffectedCalc(currentDrunkenness, 20.0f);
+                    currentDrunkenness = temp(currentDrunkenness, 20.0f);
                 }
                 break;
             case 3:
                 {
-                    currentDrunkenness = PlayerEffectedCalc(currentDrunkenness, 30.0f);
+                    currentDrunkenness = temp(currentDrunkenness, 30.0f);
                 }
                 break;
             case 4:
                 {
-                    currentDrunkenness = PlayerEffectedCalc(currentDrunkenness, 40.0f);
+                    currentDrunkenness = temp(currentDrunkenness, 40.0f);
                 }
                 break;
             case 5:
                 {
-                    currentDrunkenness = PlayerEffectedCalc(currentDrunkenness, 50.0f);
+                    currentDrunkenness = temp(currentDrunkenness, 50.0f);
                 }
                 break;
             case 6:
                 {
-                    currentDrunkenness = PlayerEffectedCalc(currentDrunkenness, 60.0f);
+                    currentDrunkenness = temp(currentDrunkenness, 60.0f);
                 }
                 break;
             case 7:
                 {
-                    currentDrunkenness = PlayerEffectedCalc(currentDrunkenness, 70.0f);
+                    currentDrunkenness = temp(currentDrunkenness, 70.0f);
                 }
                 break;
             case 8:
                 {
-                    currentDrunkenness = PlayerEffectedCalc(currentDrunkenness, 80.0f);
+                    currentDrunkenness = temp(currentDrunkenness, 80.0f);
                 }
                 break;
             case 9:
                 {
-                    currentDrunkenness = PlayerEffectedCalc(currentDrunkenness, 90.0f);
+                    currentDrunkenness = temp(currentDrunkenness, 90.0f);
                 }
                 break;
             default:
@@ -100,6 +106,42 @@ public class GameManager : MonoBehaviour {
             {
                 p_currentDrunkenness[(int)Player.DRUNK_EFFECTS.FLIP_HORI_INPUT] = true;
             }
+        }
+
+        return p_currentDrunkenness;
+    }
+
+    public bool[] temp(bool[] p_currentDrunkenness, float num)
+    {
+        if (Random.Range(0, 100) <= num)
+        {
+            int randNum = Random.Range(0, 100);
+            for(int i = 0; i < (int)Player.DRUNK_EFFECTS.EFFECT_COUNT; i++)
+            {
+                if (i != (int)Player.DRUNK_EFFECTS.FLIP_VERT_INPUT)
+                {
+                    if(effectPercentages[(int)(i)] <= randNum && randNum > effectPercentages[(int)(i - 1)])
+                    {
+                        p_currentDrunkenness[i] = true;
+                    }
+                }
+                else
+                {
+                    if (effectPercentages[(int)(i)] < randNum)
+                    {
+                        p_currentDrunkenness[i] = true;
+                    }
+                }
+            }
+
+            //if (randNum <= effectPercentages[0])
+            //{
+            //    p_currentDrunkenness[(int)Player.DRUNK_EFFECTS.FLIP_VERT_INPUT] = true;
+            //}
+            //else if (randNum <= effectPercentages[1] && randNum > effectPercentages[0])
+            //{
+            //    p_currentDrunkenness[(int)Player.DRUNK_EFFECTS.FLIP_HORI_INPUT] = true;
+            //}
         }
 
         return p_currentDrunkenness;
