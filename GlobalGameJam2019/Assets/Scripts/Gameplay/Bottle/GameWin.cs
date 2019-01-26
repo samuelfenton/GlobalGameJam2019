4d8de2f6143f6028c2b1868311450a860e8585fe
Hypr0 <eddie.ultra@gmail.com>
2019-01-26 18:16:36 +1100

    added controlling of win screen

---
+++ GlobalGameJam2019/Assets/Scripts/Gameplay/Bottle/GameWin.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameWin : MonoBehaviour
+{
+    public GameObject gWinScreenObject;
+    public GameObject bButton;
+    public ScoreManager sMan;
+    public Button bSubmitDisable;
+
+    public bool lastLevel = false;
+
+    public void Win()
+    {
+        gWinScreenObject.SetActive(true);
+        if(lastLevel == false)
+        {
+            //hide button
+            bButton.SetActive(false);
+        }
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void nextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void setPlayerName()
+    {
+        sMan.storeName(sMan.input.text);
+        sMan.setName();
+        bSubmitDisable.gameObject.SetActive(false);
+    }
+}
