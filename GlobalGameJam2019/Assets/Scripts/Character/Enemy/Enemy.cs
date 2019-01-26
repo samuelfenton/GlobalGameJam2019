14229903012666569d566d8eba8a36cde3492346
SamuelFenton <samuel.fenton@outlook.com>
2019-01-27 02:02:09 +1100

    Rearranged assets

--- GlobalGameJam2019/Assets/Scripts/Character/Enemy/Enemy.cs
+++ GlobalGameJam2019/Assets/Scripts/Character/Enemy/Enemy.cs
@@ -8,9 +8,6 @@ public class Enemy : Character
     public float m_atackPlayerDistance = 1.3f;
 
     [SerializeField]
-    private Transform m_Player;
-
-    [SerializeField]
     private NavMeshAgent m_navigationAgent;
 
     public float m_viewDistance;
@@ -19,14 +16,9 @@ public class Enemy : Character
 
     public List<GameObject> m_patrolNodes = new List<GameObject>();
 
-    private Gameover m_gameover = null;
-
     // Use this for initialization
     void Awake()
     {
-        m_Player = GameObject.FindGameObjectWithTag("Player").transform;
-
-        m_gameover = GameObject.FindGameObjectWithTag("UI").GetComponent<Gameover>();
         m_navigationAgent = GetComponent<NavMeshAgent>();
 
         m_navigationAgent.angularSpeed = Rotspeed;
