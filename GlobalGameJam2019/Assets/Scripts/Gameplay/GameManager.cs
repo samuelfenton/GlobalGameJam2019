d763cc7ed63c80e37e7b7b8932168400947eb7e4
Hypr0 <eddie.ultra@gmail.com>
2019-01-26 13:59:28 +1100

    drunk effect cycling in effect pls stand back

--- GlobalGameJam2019/Assets/Scripts/Gameplay/GameManager.cs
+++ GlobalGameJam2019/Assets/Scripts/Gameplay/GameManager.cs
@@ -12,6 +12,8 @@ public class GameManager : MonoBehaviour {
 
     public int AmountOFEffects = 3;
 
+    public bool testingMode = false;
+
     //player reference
     public Transform tPlayerPosition;
 
@@ -20,6 +22,8 @@ public class GameManager : MonoBehaviour {
     {
         //initialising the alcohol count
         nAlcoholBottles = 0;
+        if (testingMode == true)
+            nAlcoholBottles = 7;
     }
 
     //counting
@@ -37,83 +41,17 @@ public class GameManager : MonoBehaviour {
         {
             currentDrunkenness[i] = false;
         }
-        
+        int percentage = 10 * nAlcoholBottles;
+        if (percentage > 100)
+            percentage = 100;
 
-        switch (nAlcoholBottles)
-        {
-            case 1:
-                {
-                    currentDrunkenness = temp(currentDrunkenness, 10.0f);
-                }
-                break;
-            case 2:
-                {
-                    currentDrunkenness = temp(currentDrunkenness, 20.0f);
-                }
-                break;
-            case 3:
-                {
-                    currentDrunkenness = temp(currentDrunkenness, 30.0f);
-                }
-                break;
-            case 4:
-                {
-                    currentDrunkenness = temp(currentDrunkenness, 40.0f);
-                }
-                break;
-            case 5:
-                {
-                    currentDrunkenness = temp(currentDrunkenness, 50.0f);
-                }
-                break;
-            case 6:
-                {
-                    currentDrunkenness = temp(currentDrunkenness, 60.0f);
-                }
-                break;
-            case 7:
-                {
-                    currentDrunkenness = temp(currentDrunkenness, 70.0f);
-                }
-                break;
-            case 8:
-                {
-                    currentDrunkenness = temp(currentDrunkenness, 80.0f);
-                }
-                break;
-            case 9:
-                {
-                    currentDrunkenness = temp(currentDrunkenness, 90.0f);
-                }
-                break;
-            default:
-                {
-                    currentDrunkenness = temp(currentDrunkenness, 100.0f);
-                }
-                break;
-        }
+        currentDrunkenness = PlayerEffectedCalc(currentDrunkenness, percentage);
+
         return currentDrunkenness;
     }
     //the current effect
+
     public bool[] PlayerEffectedCalc(bool[] p_currentDrunkenness, float num)
-    {
-        if (Random.Range(0, 100) <= num)
-        {
-            int randNum = Random.Range(0, 100);
-            if (randNum <= effectPercentages[0])
-            {
-                p_currentDrunkenness[(int)Player.DRUNK_EFFECTS.FLIP_VERT_INPUT] = true;
-            }
-            else if(randNum <= effectPercentages[1] && randNum > effectPercentages[0])
-            {
-                p_currentDrunkenness[(int)Player.DRUNK_EFFECTS.FLIP_HORI_INPUT] = true;
-            }
-        }
-
-        return p_currentDrunkenness;
-    }
-
-    public bool[] temp(bool[] p_currentDrunkenness, float num)
     {
         //randomly geenrates 3 numebers
         int[] nRandGenNumber = new int[AmountOFEffects];
@@ -129,9 +67,9 @@ public class GameManager : MonoBehaviour {
                 int randNum = Random.Range(0, 100);
                 for (int i = 0; i < (int)Player.DRUNK_EFFECTS.EFFECT_COUNT; i++)
                 {
-                    if (i != (int)Player.DRUNK_EFFECTS.FLIP_VERT_INPUT)
+                    if (i != (int)Player.DRUNK_EFFECTS.EFFECT_COUNT - 1)
                     {
-                        if (effectPercentages[(int)(i)] <= randNum && randNum > effectPercentages[(int)(i - 1)])
+                        if (effectPercentages[(int)(i)] <= randNum && randNum < effectPercentages[(int)(i + 1)])
                         {
                             p_currentDrunkenness[i] = true;
                             break;
@@ -139,7 +77,7 @@ public class GameManager : MonoBehaviour {
                     }
                     else
                     {
-                        if (effectPercentages[(int)(i)] < randNum)
+                        if (effectPercentages[(int)(i)] <= randNum && effectPercentages[(int)(i)] < 100)
                         {
                             p_currentDrunkenness[i] = true;
                             break;
