ec4cfd4528e13bbdabf75b060fd0fb497b3e78e8
Haley <lt.knights@live.com.au>
2019-01-26 20:09:12 +1100

    build and credits

--- GlobalGameJam2019/Assets/Scripts/Gameplay/credits.cs
+++ GlobalGameJam2019/Assets/Scripts/Gameplay/credits.cs
@@ -1,8 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class credits : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject text;
+
+    [SerializeField]
+    private float speed = 2;
+
+    [SerializeField]
+    private int menuScene;
+
+    [SerializeField]
+    private float TimeEnd;
+
+    private float timel = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +26,12 @@ public class credits : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timel += Time.deltaTime;
+        text.transform.position += new Vector3(0, speed, 0);
+
+        if (timel >= TimeEnd)
+        {
+            SceneManager.LoadScene(menuScene);
+        }
 	}
 }
