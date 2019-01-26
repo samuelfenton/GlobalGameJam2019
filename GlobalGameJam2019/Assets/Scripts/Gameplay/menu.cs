ef231c2093f27f6e521b59970f0c00716f15b3c3
Haley <lt.knights@live.com.au>
2019-01-26 18:08:19 +1100

    fuck you

---
+++ GlobalGameJam2019/Assets/Scripts/Gameplay/menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+    [SerializeField]
+    private int scenestart;
+
+    [SerializeField]
+    private int scenecredits;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void StartOnClick()
+    {
+        SceneManager.LoadScene(scenestart);
+    }
+
+    public void CreditsOnClick()
+    {
+        SceneManager.LoadScene(scenecredits);
+    }
+
+    public void quitOnClick()
+    {
+        Application.Quit();
+    }
+}
