55ec5accf2ed731d797a594f55d7b96fb3e22662
SamuelFenton <samuel.fenton@outlook.com>
2019-01-26 11:05:16 +1100

    no message

--- GlobalGameJam2019/Assets/Scripts/Gameplay/LayerController.cs
+++ GlobalGameJam2019/Assets/Scripts/Gameplay/LayerController.cs
@@ -10,9 +10,9 @@ public class LayerController : MonoBehaviour
     public int m_enviromentMask = 0;
     private void Start()
     {
-        m_planeMask = LayerMask.GetMask("MousePlane");
-        m_playerMask = LayerMask.GetMask("Player");
-        m_enemyMask = LayerMask.GetMask("Enemy");
-        m_enviromentMask = LayerMask.GetMask("Enviroment");
+        m_planeMask = LayerMask.NameToLayer("MousePlane");
+        m_playerMask = LayerMask.NameToLayer("Player");
+        m_enemyMask = LayerMask.NameToLayer("Enemy");
+        m_enviromentMask = LayerMask.NameToLayer("Enviroment");
     }
 }
